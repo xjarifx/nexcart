d57631e283e02c9103273b8ea6f0019f7ea6c175
jarif <xjarifx@gmail.com>
2026-03-28 20:04:12 +0600

    Refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": ["warn", { 
-        "argsIgnorePattern": "^_",
+        "argsIgnorePattern": "^_|next",
         "varsIgnorePattern": "^_"
       }]
     }
